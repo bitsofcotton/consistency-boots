57a512c0f32c3faa53b633ab8c750ee68aec195f
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-04-09 15:44:47 +0900

    Add files via upload

--- op.sh
+++ op.sh
@@ -5,6 +5,11 @@ while [[ $off != "E" ]]; do
   ooff=`echo $off`
   off=`./incre $len $off ./bytes.h $pad`
   clang++ -O0 ./test.cc
-  echo $ooff `./a.out`
+  res=`./a.out 2>&1`
+  echo $ooff $res
+  rres=`echo $res | grep -v ok | wc -l`
+  if (( $rres == 0 )); then
+    cat ./bytes.h >> bytesall.h
+  fi
 done
 
